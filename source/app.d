66994c1ce63880264e6cbf9198891f863bf26de3
Steven Schveighoffer <schveiguy@gmail.com>
2020-12-21 11:17:27 -0500

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,65 @@
+module application;
+
+import std.stdio;
+import dparse.lexer;
+import std.file;
+
+void main(string[] args)
+{
+    auto filedata = cast(ubyte[])read(args[1]);
+    auto lexer = filedata.byToken;
+    size_t lastPosOutput = 0;
+    int importStart = -1;
+    bool hasStd = false;
+    bool foundModuleStart = false;
+    foreach(token; lexer)
+    {
+
+        // find the module statement
+        if(token == tok!"module")
+        {
+            foundModuleStart = true;
+        }
+        if(importStart != -1)
+        {
+            if(token == tok!"identifier" && token.text == "std")
+                hasStd = true;
+            if(token == tok!";")
+            {
+                if(hasStd)
+                {
+                    import std.algorithm : substitute;
+                    write(cast(char[])filedata[lastPosOutput .. importStart]);
+                    auto importstr = cast(char[])filedata[importStart .. token.index + 1];
+                    auto filtered = importstr.substitute(" std.", " ` ~ pack ~ `.");
+                    write("mixin(`", filtered, "`);");
+                    lastPosOutput = token.index + 1;
+                }
+                importStart = -1;
+            }
+        }
+        else if(token == tok!"import")
+        {
+            importStart = cast(int)token.index;
+            hasStd = false;
+        }
+        else if(foundModuleStart && token == tok!";")
+        {
+            // output everything to the semicolon, then add a newline, and insert the mixin machinery
+            foundModuleStart = false;
+            writeln(cast(char[])filedata[lastPosOutput .. token.index + 1]);
+            lastPosOutput = token.index + 1;
+            writeln(`///`);
+            writeln(`mixin _X!("std");`);
+
+            // TODO: make this work
+            // writeln(`mixin template _X(string pack):`);
+            writeln(`mixin template _X(string pack) {`);
+        }
+    }
+    write(cast(char[])filedata[lastPosOutput .. $]);
+    // close the mixin template
+    // TODO: won't need this if mixin template colon is added.
+    writeln("}");
+}
+
